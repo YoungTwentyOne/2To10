6d9b192a45bde7469343040d3fe1918a18fc6afa
YoungTwentyOne <IvanLubyanov21@gmail.com>
2026-08-25 03:07:35 +0300

    Add files via upload

---
+++ Odin.js
@@ -0,0 +1,31 @@
+function Resultat(el) {
+        var dano = el.Choose1.value;
+        var resultat = el.Choose2.value;
+        var iz = el.System.value;
+        var v = el.System2.value;
+        if(iz == "Двоичная")
+         var iz2 = 2;
+        else if(iz == "Восьмиричная")
+         var iz2 = 8;
+        else if(iz == "Десятиричная")
+         var iz2 = 10;
+        else if(iz == "Шестнадцатиричная")
+         var iz2 = 16;
+
+         if(v == "Двоичная")
+         var v2 = 2;
+        else if(v == "Восьмиричная")
+         var v2 = 8;
+        else if(v == "Десятиричная")
+         var v2 = 10;
+        else if(v == "Шестнадцатиричная")
+         var v2 = 16;
+
+         resultat = parseInt(String(dano), iz2);
+         var number = resultat.toString(v2);
+         el.Choose2.value = number;
+         
+        
+
+        return false;
+    }
